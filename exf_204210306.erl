9b33fadae0914c82e25bd1f4b669b7f88377c3a4
amitnh <amitnh@gmail.com>
2020-05-05 17:22:18 +0200

    reducedBool fix

--- exf_204210306.erl
+++ exf_204210306.erl
@@ -11,7 +11,7 @@
 %% API
 -export([setVar/3,reduceBool/1]).
 
-%---------------------------------------------------------------------------------------------------------------------------------------------------
+%----------------------------------------------------------------------------------------------```-----------------------------------------------------
 %---- setVar: sets a value to an Argument,  for example: setVar(x1,true,{and,x1,x2}) -> {and,1,x2}
 setVar(Var,Value,BooleanExp) when (Value ==  1) or (Value == true) -> setA(Var,1,BooleanExp) ;
 setVar(Var,Value,BooleanExp) when (Value ==  0) or (Value == false) -> setA(Var,0,BooleanExp);
@@ -36,7 +36,7 @@ setA(Var,V,BooleanExp) when is_tuple(BooleanExp)  -> if % V is 0/1
                                                     end;
 setA(_,_,BooleanExp) -> BooleanExp. % in case its not a tuple its a Var.
 %---------------------------------------------------------------------------------------------------------------------------------------------------
-%---- reduce the BooleanExp: for exp: reduceBool({and,0,x2}) -> 0;
+%---- reduce the BooleanExp: for exp: reduceBool({and,0,x2}) -> 0; reduceBool({and,1,x2}) -> x2
 reduceBool(BooleanExp) when not is_tuple(BooleanExp)-> BooleanExp;
 reduceBool(BooleanExp) when element(1, BooleanExp) == 'not'-> if  is_tuple(element(2,BooleanExp)) -> reduceBool({'not',reduceBool(element(2,BooleanExp))});
                                                                 element(2,BooleanExp) == 1 -> 0;
@@ -45,12 +45,16 @@ reduceBool(BooleanExp) when element(1, BooleanExp) == 'not'-> if  is_tuple(eleme
                                                               end;
 reduceBool(BooleanExp) when element(1, BooleanExp) == 'and'-> if (element(2,BooleanExp) == 0) or (element(3,BooleanExp) == 0)  -> 0;
                                                                 (element(2,BooleanExp) == 1) and (element(3,BooleanExp) == 1) -> 1;
+                                                                (element(2,BooleanExp) == 1) -> reduceBool(element(3,BooleanExp));
+                                                                (element(3,BooleanExp) == 1) -> reduceBool(element(2,BooleanExp));
                                                                 is_tuple(element(2,BooleanExp)) -> reduceBool({'and',reduceBool(element(2,BooleanExp)),element(3,BooleanExp)});
                                                                 is_tuple(element(3,BooleanExp)) ->  reduceBool({'and',element(2,BooleanExp),reduceBool(element(3,BooleanExp))});
                                                                 true -> BooleanExp
                                                               end;
 reduceBool(BooleanExp) when element(1, BooleanExp) == 'or'-> if (element(2,BooleanExp) == 1) or (element(3,BooleanExp) == 1)  -> 1;
                                                                 (element(2,BooleanExp) == 0) and (element(3,BooleanExp) == 0)  -> 0;
+                                                                (element(2,BooleanExp) == 0) -> reduceBool(element(3,BooleanExp));
+                                                                (element(3,BooleanExp) == 0) -> reduceBool(element(2,BooleanExp));
                                                                 is_tuple(element(2,BooleanExp)) -> reduceBool({'or',reduceBool(element(2,BooleanExp)),element(3,BooleanExp)});
                                                                 is_tuple(element(3,BooleanExp)) ->  reduceBool({'or',element(2,BooleanExp),reduceBool(element(3,BooleanExp))});
                                                                 true -> BooleanExp
