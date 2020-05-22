762e56eea61ba7a1817065c282067e39db495dba
amitnh <amitnh@gmail.com>
2020-05-22 15:15:04 +0200

    ex5first not even close

--- ex5_204210306.erl
+++ ex5_204210306.erl
@@ -11,24 +11,51 @@
 %% API
 -export([ring_parallel/2]).
 %---------------------------------------------------------------
-% creates N processes thats point to one another.
-ring_parallel(N,M)->ring_parallel(N,N,M).
-ring_parallel(N,I,M)->spawn(ring_parallel_spawn(N,I,M,self(),self())).
+%%% creates N processes thats point to one another.
+%%ring_parallel(N,M)->ring_parallel(N,N,M).
+%%ring_parallel(N,I,M)->spawn(ring_parallel_spawn(N,I,M,self(),self(),os:timestamp())).
+%%
+%%%creating the next chain and going into a recieve loop
+%%ring_parallel_spawn(_,1,M,To,First,StartTime)-> First ! {to,self()} ,ring_parallel_First(M,0,To,StartTime); %end of recursion. close the circle
+%%ring_parallel_spawn(N,I,M,To,First,StartTime)-> ring_parallel_spawn(N,I-1,M,self(),First,StartTime), spawn(ring_parallel_loop(To)).
+%%
+%%%sends M messages from First node
+%%ring_parallel_First(M,0,_,StartTime)->io:format("Total Time of Function: ~f miliseconds~n", [timer:now_diff(os:timestamp(), StartTime) / 1000]);
+%%ring_parallel_First(M,ToReceive,_,StartTime)->receive
+%%                              {M}->ring_parallel_First(M,ToReceive-1,0,StartTime)
+%%                            end;
+%%ring_parallel_First(M,ToReceive,To,StartTime)->
+%%
+%%  To ! {M}, ring_parallel_First(M-1,ToReceive,To,StartTime).
+%%
+%%
+%%ring_parallel_loop(To)->
+%%  receive
+%%    {Message}-> To ! Message
+%%  end.
+%----------------------------------------------------------------------------
+ring_parallel(N,M)->MesMap = makeMesMap(N).
 
-%creating te next chain and going into a recieve loop
-ring_parallel_spawn(N,1,M,To,First)->First ! {to,self()} ,ring_parallel_sendMessages(M,To); %end of recursion. close the circle
-ring_parallel_spawn(N,I,M,To,First)-> ring_parallel_spawn(N,I-1,M,self(),First), spawn(ring_parallel_loop(To)).
+%makes a List with N empty maps.
+makeMesMap(N)-> makeMesMap(N,[]).
+makeMesMap(0,MesMap)->MesMap;
+makeMesMap(N,MesMap)->makeMesMap(N-1,[#{}] ++ MesMap).
 
-%sends M messages trow First node
-ring_parallel_sendMessages(0,To)->time;
-ring_parallel_sendMessages(M,To)->
-  To ! {M},
-  ring_parallel_sendMesssages(M-1,To).
-
-
-ring_parallel_loop(To)->
+%node_loop- each process is a node
+% ToList - list of nodes to send to
+% C- unique number
+%MesMap- List of maps, each node gets a map, thats saves his recieved messages
+node_loop(ToList,C,MesMap)->
   receive
-    {to,NewTo}->ring_parallel_loop(NewTo);
-    {Message}-> To ! Message;
-    _-> ring_parallel_loop(To)
+    {Message} -> if
+                   maps:is_key(Message,lists:nth(C,MesMap)) -> sendMes(ToList,Message);
+                   true-> banana
+                 end;
+    {addToList,Pid} -> node_loop(ToList ++ [Pid],C,MesMap);
+    _-> node_loop(ToList,C,MesMap)
   end.
+
+node_loop_master(ToList,C)->aaa.
+
+sendMes([],Message)-> io:format("Message: \"~f\" sent.~n", [Message]);
+sendMes([H|ToList],Message)-> H ! Message,  sendMes(ToList,Message).
