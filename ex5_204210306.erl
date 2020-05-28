b1de39bd97f2b79b42741c23f08a0251bf1ca08b
amitnh <amitnh@gmail.com>
2020-05-28 12:58:38 +0200

    ex5 works ! tuple return ok

--- ex5_204210306.erl
+++ ex5_204210306.erl
@@ -16,19 +16,20 @@
 %----------------------------------------------------------------------------
 
 ring_parallel(1,_) -> notAcircle;
-ring_parallel(N,M) when is_integer(N) and is_integer(M)-> register(node1,spawn(fun()->ring_parallel(N,N,M,node1) end));
+ring_parallel(N,M) when is_integer(N) and is_integer(M)-> Self= self(), register(node1,spawn(fun()->ring_parallel(N,N,M,node1,Self) end)),answer_loop();
 ring_parallel(_,_)-> badArguments.
 
-ring_parallel(_,1,M,_) -> node_loop_master([node2],[],M,0,0,os:timestamp()); %close the loop : node1---->node2
-ring_parallel(N,I,M,Last) -> register(Node=numToAtom(I),spawn(fun()->node_loop([Last],I,[]) end)),ring_parallel(N,I-1,M,Node). % makes N processes node1,node2,...,nodeN.
+ring_parallel(_,1,M,_,TopPid) ->  TopPid ! node_loop_master([node2],[],M,0,0,os:timestamp()); %close the loop : node1---->node2
+ring_parallel(N,I,M,Last,TopPid) -> register(Node=numToAtom(I),spawn(fun()->node_loop([Last],I,[]) end)),ring_parallel(N,I-1,M,Node,TopPid). % makes N processes node1,node2,...,nodeN.
 
-
+answer_loop()->   receive Msg-> flush(),Msg end.
+flush() -> receive _ ->flush() after 0 -> ok end.
 
 %sends M msgs
 node_loop_master(ToList,_,M,M,M,StartTime)-> sendMes(ToList,close), %recieved all msgs
       receive
         {_,_,close} -> io:format("[node1] and All other processes are closed ~n"),
-          io:format("Total Time of Function: ~f miliseconds~n", [timer:now_diff(os:timestamp(), StartTime) / 1000]), {timer:now_diff(os:timestamp(), StartTime),M,M}%waits for the {close} message back
+          io:format("Total Time of Function: ~f miliseconds~n", [Time=timer:now_diff(os:timestamp(), StartTime) / 1000]), {Time,M,M}%waits for the {close} message back
       end;
 node_loop_master(ToList,History,M,M,Recieved,StartTime)->
       receive
@@ -92,21 +93,21 @@ atomToNum(Atom)->list_to_integer(string:substr(atom_to_list(Atom),5)).
 %----------------------------------------------------------------------------
 %----------------------------------------------------------------------------
 ring_serial(1,_) -> notAcircle;
-ring_serial(V,M) when is_integer(V) and is_integer(M)-> spawn(fun() -> ring_serial(1,V+1,0,M,os:timestamp()) end);
+ring_serial(V,M) when is_integer(V) and is_integer(M)->Self=self(), spawn(fun() -> ring_serial(1,V+1,0,M,os:timestamp(),Self) end),answer_loop();
 ring_serial(_,_)-> badArguments.
 
 % i similate the circle
 % Vertex1 and vertexN is the sameone
-ring_serial(_,V,M,M,StartTime)->
+ring_serial(_,V,M,M,StartTime,Self)->
                       receive
-                         {_,V,M}->  io:format("Total Time of Function: ~f miliseconds~n", [timer:now_diff(os:timestamp(), StartTime) / 1000]), {timer:now_diff(os:timestamp(), StartTime),M,M}; % if im the last msg
-                         {_,Me,M}-> ring_serial(Me,V,1,M,StartTime);% if im the last msg for this vertex
-                         {_,Me,_}-> ring_serial(Me,V,M,M,StartTime); %middle of recieving msgs
+                         {_,V,M}->  io:format("Total Time of Function: ~f miliseconds~n", [Time = timer:now_diff(os:timestamp(), StartTime) / 1000]), Self ! {Time,M,M}; % if im the last msg
+                         {_,Me,M}-> ring_serial(Me,V,1,M,StartTime,Self);% if im the last msg for this vertex
+                         {_,Me,_}-> ring_serial(Me,V,M,M,StartTime,Self); %middle of recieving msgs
                          _ -> error
                        end;
-ring_serial(Me,V,Sent,M,StartTime) -> self() ! {Me,Me+1,Sent+1}
+ring_serial(Me,V,Sent,M,StartTime,Self) -> self() ! {Me,Me+1,Sent+1}
   %,io:format("Message: ~p sent from: vertex~p to: vertex~p ~n", [Sent,Me,Me+1])
-  ,ring_serial(Me,V,Sent+1,M,StartTime).
+  ,ring_serial(Me,V,Sent+1,M,StartTime,Self).
 %----------------------------------------------------------------------------
 %----------------------------------------------------------------------------
 %----------------------------------------------------------------------------
@@ -115,12 +116,12 @@ ring_serial(Me,V,Sent,M,StartTime) -> self() ! {Me,Me+1,Sent+1}
 %----------------------------------------------------------------------------
 
 mesh_parallel(1,_,_)-> chooseNBiggerThen1;
-mesh_parallel(N,M,C) when is_integer(N) and is_integer(M) and is_integer(C) and (C>0) and (C<N*N +1) ->register(numToAtom(C),spawn(fun()->mesh_parallel(N,N*N,M,C) end));
+mesh_parallel(N,M,C) when is_integer(N) and is_integer(M) and is_integer(C) and (C>0) and (C<N*N +1) ->Self= self(), register(numToAtom(C),spawn(fun()->mesh_parallel(N,N*N,M,C,Self) end)), answer_loop();
 mesh_parallel(_,_,_)-> badArguments.
 
-mesh_parallel(N,0,M,C) -> node_loop_master_mesh(getNeighborsMesh(C,N),[],M,0,0,(((N*N)-1)*M),os:timestamp()); %mesh master does that- number C
-mesh_parallel(N,C,M,C) -> mesh_parallel(N,C-1,M,C); % skip building C
-mesh_parallel(N,I,M,C) -> register(numToAtom(I),spawn(fun()->node_loop(getNeighborsMesh(I,N),I,[]) end)),mesh_parallel(N,I-1,M,C). % makes N processes node1,node2,...,nodeN. %,io:format("build node~p ~n",[I])
+mesh_parallel(N,0,M,C,Self) -> node_loop_master_mesh(getNeighborsMesh(C,N),[],M,0,0,(((N*N)-1)*M),os:timestamp(),Self); %mesh master does that- number C
+mesh_parallel(N,C,M,C,Self) -> mesh_parallel(N,C-1,M,C,Self); % skip building C
+mesh_parallel(N,I,M,C,Self) -> register(numToAtom(I),spawn(fun()->node_loop(getNeighborsMesh(I,N),I,[]) end)),mesh_parallel(N,I-1,M,C,Self). % makes N processes node1,node2,...,nodeN. %,io:format("build node~p ~n",[I])
 
 %get the neighbors list, gets the N and the index I,
 % return list of neighbors nodes for example: getneighborsMesh(2,10)-> [node1,node3,node12]
@@ -132,41 +133,42 @@ getAllNodes(N)-> [numToAtom(X)||X<-lists:seq(1, N*N)].
 
 
 %sends M msgs
-node_loop_master_mesh(_,_,M,M,ToRecieve,ToRecieve,StartTime)-> sendMes(getAllNodes((ToRecieve div M) +1) -- [pidToRegName(self())], {master,close}), %recieved all msgs getAllNodes((ToRecieve div M) +1) -- [pidToRegName(self())]
-     EndTime=os:timestamp(), io:format("Total Time of Function: ~f miliseconds~n", [timer:now_diff(EndTime, StartTime) / 1000]), {timer:now_diff(EndTime, StartTime),M,M};%waits for the {close} message back  sleep(2000),unregisterAll((ToRecieve div M))
+node_loop_master_mesh(_,_,M,M,ToRecieve,ToRecieve,StartTime,Self)-> sendMes(getAllNodes((ToRecieve div M) +1) -- [pidToRegName(self())], {master,close}), %recieved all msgs getAllNodes((ToRecieve div M) +1) -- [pidToRegName(self())]
+     EndTime=os:timestamp(), io:format("Total Time of Function: ~f miliseconds~n", [timer:now_diff(EndTime, StartTime) / 1000]), Self ! {timer:now_diff(EndTime, StartTime),M,M};%waits for the {close} message back  sleep(2000),unregisterAll((ToRecieve div M))
 
-node_loop_master_mesh(ToList,History,M,M,Recieved,ToRecieve,StartTime)-> %wating to recieve all the msgs back
+node_loop_master_mesh(ToList,History,M,M,Recieved,ToRecieve,StartTime,Self)-> %wating to recieve all the msgs back
   receive
     {_,_,close} -> io:format("C and All other processes are closed ~n"),
-      io:format("Total Time of Function: ~f miliseconds~n", [timer:now_diff(os:timestamp(), StartTime) / 1000]), {timer:now_diff(os:timestamp(), StartTime),M,Recieved};
-    {_,_, {master,_}}->node_loop_master_mesh(ToList,History,M,M,Recieved,ToRecieve,StartTime);
+      io:format("Total Time of Function: ~f miliseconds~n", [Time= timer:now_diff(os:timestamp(), StartTime) / 1000]), {Time,M,Recieved};
+    {_,_, {master,_}}->node_loop_master_mesh(ToList,History,M,M,Recieved,ToRecieve,StartTime,Self);
       {_,_, {Atom,Message}}-> IsMember= lists:member({Atom, Message},History), %check if ive send that msg in the past
                   if not IsMember ->% 1st time i receieved this msg
-                    node_loop_master_mesh(ToList,History++[{Atom, Message}],M,M,Recieved+1,ToRecieve,StartTime);
-                    true->node_loop_master_mesh(ToList,History,M,M,Recieved,ToRecieve,StartTime)
+                    node_loop_master_mesh(ToList,History++[{Atom, Message}],M,M,Recieved+1,ToRecieve,StartTime,Self);
+                    true->node_loop_master_mesh(ToList,History,M,M,Recieved,ToRecieve,StartTime,Self)
                   end;
 
-    _ -> node_loop_master_mesh(ToList,History,M,M,Recieved,ToRecieve,StartTime)
+    _ -> node_loop_master_mesh(ToList,History,M,M,Recieved,ToRecieve,StartTime,Self)
   end;
-node_loop_master_mesh(ToList,History,M,Sent,Recieved,ToRecieve,StartTime)-> sendMes(ToList, {master,Sent}),node_loop_master_mesh(ToList,History,M,Sent+1,Recieved,ToRecieve,StartTime).
+node_loop_master_mesh(ToList,History,M,Sent,Recieved,ToRecieve,StartTime,Self)-> sendMes(ToList, {master,Sent}),node_loop_master_mesh(ToList,History,M,Sent+1,Recieved,ToRecieve,StartTime,Self).
 
 %----------------------------------------------------------------------------
 %----------------------------------------------------------------------------
 %----------------------------------------------------------------------------
 
-mesh_serial(N,M,C) when is_integer(N) and is_integer(M) and (C>0) and (C<N*N+1) ->
+mesh_serial(N,M,C) when is_integer(N) and is_integer(M) and (C>0) and (C<N*N+1) -> Self= self(),
   spawn(fun() ->
   mesh_serial(N,C,0,M,
     ((N*N)-1)*M, % ToReceive
     [[]||_<-lists:seq(1,(N*N))], %N*N empty lists in a list. for History
-    os:timestamp())
-        end); %
+    os:timestamp(),Self)
+        end),
+answer_loop(); % wait for answer
 mesh_serial(_,_,_)->badArguments.
 
 
 %history is map N*N maps for each vertex
-mesh_serial(_,_,M,M,0,_,StartTime)->io:format("Total Time of Function: ~f miliseconds~n", [timer:now_diff(os:timestamp(), StartTime) / 1000]), {timer:now_diff(os:timestamp(), StartTime),M,M}; % if im the last msg
-mesh_serial(N,C,M,M,ToReceive,History,StartTime)->
+mesh_serial(_,_,M,M,0,_,StartTime,Self)-> Time = timer:now_diff(os:timestamp(), StartTime),io:format("Total Time of Function: ~f miliseconds~n", [Time / 1000]), Self ! {Time,M,M}; % if im the last msg
+mesh_serial(N,C,M,M,ToReceive,History,StartTime,Self)->
   receive
     {master,X,Msg}->
       List=[lists:nth(X,History)],
@@ -176,33 +178,33 @@ mesh_serial(N,C,M,M,ToReceive,History,StartTime)->
 
                                       [self() ! {X,Y,Msg} ||Y<-Neighbors], %new msg
 
-                                      mesh_serial(N,C,M,M,ToReceive,updateHistory(updateHistory(History,X,{master,X,Msg}),X,{X,Msg}),StartTime);
-                      true-> mesh_serial(N,C,M,M,ToReceive,History,StartTime)
+                                      mesh_serial(N,C,M,M,ToReceive,updateHistory(updateHistory(History,X,{master,X,Msg}),X,{X,Msg}),StartTime,Self);
+                      true-> mesh_serial(N,C,M,M,ToReceive,History,StartTime,Self)
                     end;
     {Z,C,Msg}->
       List= [lists:nth(C,History)],
                 IsMember = lists:member({Z,Msg},List), %normal msg, pass it if u havent yet
-                if not IsMember -> mesh_serial(N,C,M,M,ToReceive-1,updateHistory(History,C,{Z,Msg}),StartTime); %recieved a new msg
-                true-> mesh_serial(N,C,M,M,ToReceive,History,StartTime) %recieved a old msg
+                if not IsMember -> mesh_serial(N,C,M,M,ToReceive-1,updateHistory(History,C,{Z,Msg}),StartTime,Self); %recieved a new msg
+                true-> mesh_serial(N,C,M,M,ToReceive,History,StartTime,Self) %recieved a old msg
                 end;
     {Z,X,Msg}->
       IsMember = lists:member({Z,Msg},[lists:nth(X,History)]), %normal msg, pass it if u havent yet
                   if not IsMember ->Neighbors=[atomToNum(Y)||Y<-getNeighborsMesh(X,N)], [self() ! {Z,Y,Msg} ||Y<-Neighbors], %first time to receive msg
                     [self() ! {Z,Y,Msg} ||Y<-Neighbors],
 
-                    mesh_serial(N,C,M,M,ToReceive,updateHistory(History,X,{Z,Msg}),StartTime);
-                    true-> mesh_serial(N,C,M,M,ToReceive,History,StartTime)
+                    mesh_serial(N,C,M,M,ToReceive,updateHistory(History,X,{Z,Msg}),StartTime,Self);
+                    true-> mesh_serial(N,C,M,M,ToReceive,History,StartTime,Self)
                   end;
     _ -> error
   end;
 
 
 
-mesh_serial(N,C,I,M,ToReceive,History,StartTime) ->
+mesh_serial(N,C,I,M,ToReceive,History,StartTime,Self) ->
 Neighbors=[atomToNum(X)||X<-getNeighborsMesh(C,N)], %list of numbers of neighbors: [2,5,3,7].
   [self() ! {master,X,I} ||X<-Neighbors],% send a Msg to each Neighbors
 
-            mesh_serial(N,C,I+1,M,ToReceive,History,StartTime). % recursion
+            mesh_serial(N,C,I+1,M,ToReceive,History,StartTime,Self). % recursion
 
 
 %insert Msg to the History Matrix
