0fa3b3f531ef18739b514cc1942dd733ab92de36
amitnh <amitnh@gmail.com>
2020-05-29 16:28:29 +0200

    ex7 upload finished !

--- ex7_204210306.erl
+++ ex7_204210306.erl
@@ -12,26 +12,26 @@
 %% API
 -export([steady/1,calc/3]).
 %---------------------------------------------------------------
-%steady(F):
-% takes a function F() and trying to do it
-% in either way it will be written to myLog_204210306.elog with a time stamp
+% steady(F):
+% takes a function F() and trys to run it.
+% in either way, success or failue, it will be written to myLog_204210306.elog with a time stamp
 %---------------------------------------------------------------
 steady(F) ->
-  file:open("myLog_204210306.elog",[read, write, append]),
+  file:open("myLog_204210306.elog",[write, append]),
   try F() of
     %case of success: {time, success, ReturnValue}
     RetVal -> file:write_file("myLog_204210306.elog", io_lib:format("{~p,success,~p} ~n", [os:system_time(second), RetVal]), [append]),RetVal
   catch
     % case of error: {time, error, Error}
-    error:Error -> file:write_file("myLog_204210306.elog",io_lib:format("{~p,error,~p} ~n", [os:system_time(second), Error]), [append]), Error;
+    error:Error -> file:write_file("myLog_204210306.elog",io_lib:format("{~p,error,~p} ~n", [os:system_time(second), Error]), [append]),Error;
     % case of exit: {time, exit, Exit}
-    exit:Exit -> file:write_file("myLog_204210306.elog",io_lib:format("{~p,exit,~p} ~n", [os:system_time(second), Exit]), [append]), Exit;
+    exit:Exit -> file:write_file("myLog_204210306.elog",io_lib:format("{~p,exit,~p} ~n", [os:system_time(second), Exit]), [append]),Exit;
     % case of trow: {time, throw, Throw}
-    throw:Throw -> file:write_file("myLog_204210306.elog",io_lib:format("{~p,throw,~p} ~n", [os:system_time(second), Throw]), [append]), Throw
+    throw:Throw -> file:write_file("myLog_204210306.elog",io_lib:format("{~p,throw,~p} ~n", [os:system_time(second), Throw]), [append]),Throw
   end.
 
 %---------------------------------------------------------------
-%scalc(division,A,B):
+% calc(division,A,B):
 % trys to do A/B
 % it will fail when B=0, but we will catch the Error with the catch
 %---------------------------------------------------------------
