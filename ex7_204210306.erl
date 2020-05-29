de62533dc9da458b20b8cb8b5c2e8ff2007d5fc9
amitnh <amitnh@gmail.com>
2020-05-29 16:16:50 +0200

    ex7 upload finished?! prob...

--- ex7_204210306.erl
+++ ex7_204210306.erl
@@ -16,18 +16,18 @@ steady(F) ->
   file:open("myLog_204210306.elog",[read, write, append]),
   try F() of
     %case of success: {time, success, ReturnValue}
-    RetVal -> file:write_file("myLog_204210306.elog", io_lib:format("{~p,success,~p} ~n", [erlang:localtime(), RetVal]), [append]),RetVal
+    RetVal -> file:write_file("myLog_204210306.elog", io_lib:format("{~p,success,~p} ~n", [os:system_time(second), RetVal]), [append]),RetVal
   catch
     % case of error: {time, error, Error}
-    error:Error -> file:write_file("myLog_204210306.elog",io_lib:format("{~p,error,~p} ~n", [erlang:localtime(), Error]), [append]), Error;
+    error:Error -> file:write_file("myLog_204210306.elog",io_lib:format("{~p,error,~p} ~n", [os:system_time(second), Error]), [append]), Error;
     % case of exit: {time, exit, Exit}
-    exit:Exit -> file:write_file("myLog_204210306.elog",io_lib:format("{~p,exit,~p} ~n", [erlang:localtime(), Exit]), [append]), Exit;
+    exit:Exit -> file:write_file("myLog_204210306.elog",io_lib:format("{~p,exit,~p} ~n", [os:system_time(second), Exit]), [append]), Exit;
     % case of trow: {time, throw, Throw}
-    throw:Throw -> file:write_file("myLog_204210306.elog",io_lib:format("{~p,throw,~p} ~n", [erlang:localtime(), Throw]), [append]), Throw
+    throw:Throw -> file:write_file("myLog_204210306.elog",io_lib:format("{~p,throw,~p} ~n", [os:system_time(second), Throw]), [append]), Throw
   end.
 
 calc(division,A,B)->try A/B of
                       Val-> Val
                     catch
-                      error:Error -> divisionByZero
+                      Exceptione:E -> {os:system_time(second),Exceptione, divisionByZero,E}
                     end.
