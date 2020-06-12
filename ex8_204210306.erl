49621c21acf0aee190068425d9e5a6ff09865347
amitnh <amitnh@gmail.com>
2020-06-12 14:45:36 +0200

    ex8 almost working

--- ex8_204210306.erl
+++ ex8_204210306.erl
@@ -10,39 +10,25 @@
 
 
 %% API
--export([startChat/1,call/1,send/1,steadyLink/1,steadyMon/1]).
+-export([remoteStartChat/1,startChat/1,call/1,send/1,steadyLink/1,steadyMon/1]).
 %---------------------------------------------------------------
 %%startChat()->Pid
 %---------------------------------------------------------------
 startChat(RemoteName@IP)->
   case whereis(localPID) of
     undefined ->
-      register(localPID, Pid_Local_Sender = spawn(fun() -> loop(RemoteName@IP, 0, 0) end)), %spawn process for local
+      register(localPID, Pid_Local_Sender = spawn(fun() -> localLoop(RemoteName@IP, 0, 0) end)), %spawn process for local
       rpc:call(RemoteName@IP, ex8_204210306, remoteStartChat, [node()]), %call a function thats spawn process for remote
-      put(remoteName@IP, RemoteName@IP),   % Saves RemoteName@IP
+      put(remoteName@IP, RemoteName@IP), % Saves RemoteName@IP
       Pid_Local_Sender; %returns the pid
     _ -> io:format("chat already works")
   end.
 
-
-remoteStartChat(LocalName@IP) ->
-  % check if we already put it
-  case whereis(remotePID) of
-    undefined ->
-      register(remotePID, spawn(fun() -> remoteLoop(LocalName@IP, 0, 0) end));
-    _ -> io:format("chat already works")
-  end.
-
-send(Message) -> remotePID ! Message.
-
-call(Message)-> rpc:call(get(remoteName@IP), ex8_204210306, send, [Message]).
-
-
-loop(RemoteName@IP, Sent, Received)->
+localLoop(RemoteName@IP, Sent, Received)->
   receive
     stats ->
       io:format("Local stats: sent: ~p received: ~p~n", [Sent, Received]),
-      loop(RemoteName@IP, Sent, Received);
+      localLoop(RemoteName@IP, Sent, Received);
 
     quit ->
       {remotePID, RemoteName@IP} ! quit,
@@ -50,31 +36,47 @@ loop(RemoteName@IP, Sent, Received)->
       exit(requested);
 
     {fromRemote, Message} ->
+      io:format("~p - local recieved msg from remote .~n",[Message]),
       Message,
-      loop(RemoteName@IP, Sent, Received + 1);
+      localLoop(RemoteName@IP, Sent, Received + 1);
 
     Message ->
-      {remotePID, RemoteName@IP} ! {fromLocal, Message},
-      loop(RemoteName@IP, Sent + 1, Received)
+      {remotePID,RemoteName@IP} ! {fromLocal, Message},
+      io:format("~p - local recieved msg .~n",[Message]),
+      localLoop(RemoteName@IP, Sent + 1, Received)
+  end.
+
+call(Message)-> rpc:call(get(remoteName@IP), ex8_204210306, send, [Message]).%
+send(Message)-> remotePID ! Message.
+
+remoteStartChat(LocalName@IP) ->
+  % check if we already put it
+  case whereis(remotePID) of
+    undefined ->
+      register(remotePID, spawn(fun() -> remoteLoop(LocalName@IP, 0, 0) end));
+    _ -> io:format("chat already works~n")
   end.
 
 
-remoteLoop(LocalName@IP, SentIndex, ReceivedIndex) ->
+remoteLoop(LocalName@IP, Sent, Received) ->
   receive
     stats ->
-      io:format("remote stats: sent: ~p received: ~p~n", [SentIndex, ReceivedIndex]),
-      remoteLoop(LocalName@IP, SentIndex, ReceivedIndex);
+      io:format("remote stats: sent: ~p received: ~p~n", [Sent, Received]),
+      remoteLoop(LocalName@IP, Sent, Received);
 
     quit->
+      {localPID, LocalName@IP} ! quit,
       io:format("~p - Successfully closed.~n",[self()]),
       exit(requested);
 
-    {fromLocal, _}->
-      remoteLoop(LocalName@IP, SentIndex, ReceivedIndex + 1);
+    {fromLocal, Message}->
+      io:format("~p - remote recieved msg from local.~n",[Message]),
+      remoteLoop(LocalName@IP, Sent, Received + 1);
 
     Message ->
+      io:format("~p - remote recieved msg .~n",[Message]),
       {localPID, LocalName@IP} ! {fromRemote, Message},
-      remoteLoop(LocalName@IP, SentIndex + 1, ReceivedIndex)
+      remoteLoop(LocalName@IP, Sent + 1, Received)
   end.
 %--------------------------------------------------------------------------------------------
 % Spawns a process to evaluate function F/0, Links the two processes
@@ -86,32 +88,18 @@ steadyLink(F) when is_function(F)-> Pid = spawn_link(F),
 steadyLink(_)-> notAfunction.
 %--------------------------------------------------------------------------------------------
 steadyMon(F) ->
-  spawn_monitor(ex8_204210306, steadyEval, [F]),
+  spawn_monitor(fun()->F()end),
   % Wait for termination message from the above process
   receive
   % Normal termination
     {_,_,_, PID, normal} ->
       "Normal termination of process " ++ getPID(PID) ++ " was detected";
-
   % Termination with an a exception
     {_,_,_, PID, Info} ->
-      "An exception in process " ++ getPID(PID) ++ " was detected: " ++ atom_to_list(Info)
+      "An exception in process " ++ getPID(PID) ++ " was detected: " ++ Info
   after 5000 -> ok
   end.
 
 %% getPID(PID)
-%  Extract the x.y.z PID number from termination message PID component
 getPID(PID)->
   string:sub_string(pid_to_list(PID), 2, (string:len(pid_to_list(PID))-1)).
-
-%% steadyEval(F)
-% aid function, evalute the function F on the new process that have had been monitored
-% <ExceptionType> is one of the following: error, exit or throw
-steadyEval(F) ->
-  try F() of
-    _	-> normalTermination
-  catch
-    error:_Error 	-> exit(error);
-    exit:_Exit	-> exit(exit);
-    throw:_Throw	-> exit(throw)
-  end.
