8f37e192924135750d7fb3d1cfa53474bd8d24ef
amitnh <amitnh@gmail.com>
2020-06-22 11:41:41 +0200

    ex9 start

--- ex9_204210306.erl
+++ ex9_204210306.erl
@@ -5,16 +5,31 @@
 %%%
 %%% @end
 %%%-------------------------------------------------------------------
--module(ex8_204210306).
+-module(ex9_204210306).
 -author("amit").
 
 %% API
--export([]).
+-export([etsBot/0]).
 %---------------------------------------------------------------
 %%startChat()->Pid
 %---------------------------------------------------------------
-file:open("myLog_204210306.elog",[write, append]),
-try F() of
-%case of success: {time, success, ReturnValue}
-RetVal -> file:write_file("myLog_204210306.elog", io_lib:format("{~p,success,~p} ~n", [os:system_time(second), RetVal]), [append]),RetVal
-catch
+etsBot()->   {_, File} = file:open("etsCommands.txt",[read]),
+  [Type|Actions]= readFile(File),
+  makeEts(Type),
+  doActions(Actions),
+  printEts().
+
+makeEts(Type)-> try ets:new(botEts, [list_to_atom(Type -- "\n"), named_table]) catch _-> errorWrongType end.
+
+ % printEts().
+%%printEts()-> file:open("etsRes_204210306.ets",[write, append]),
+%%  file:write_file("etsRes_204210306.ets", io_lib:format("~p ~p~n", [Key, Value]), [append]).
+
+%---------- readlines, open file and return the String inside
+readFile(File) ->
+  case file:read_line(File) of
+    % Data
+    {ok, Data} -> [Data | readFile(File)];
+    % End of file
+    eof -> []
+  end.
